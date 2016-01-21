3b7089591dc0d690a0289b1c44b67c04e05af708
Emily Horsman <me@emilyhorsman.com>
2016-01-21 16:27:13 -0500

    Switch from gen_random_uuid to uuid_generate_v4
    Would like to use pgcrypto but gen_random_uuid is only available in
    Postgresql 9.4. Travis CI only has up to 9.3, and I have no guarantee
    that platforms I'll be deploying to in the future have 9.4.

--- sql/1453331350_migration.sql
+++ sql/1453331350_migration.sql
@@ -2,7 +2,6 @@ DROP SCHEMA IF EXISTS nightshades CASCADE;
 CREATE SCHEMA nightshades;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE nightshades.users (
   id   bigserial PRIMARY KEY,
@@ -10,7 +9,7 @@ CREATE TABLE nightshades.users (
 );
 
 CREATE TABLE nightshades.units (
-  id          uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  id          uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   user_id     integer REFERENCES nightshades.users(id),
   completed   boolean DEFAULT FALSE NOT NULL,
   start_time  timestamp with time zone NOT NULL DEFAULT NOW(),
