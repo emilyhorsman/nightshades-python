12ade776ae22979b6662f221fe1057c1b87bf283
Emily Horsman <me@emilyhorsman.com>
2016-01-20 19:37:13 -0500

    Let's make a Python package!

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nightshades',
+      packages=['nightshades'],
+      version='0.1',
+      description='A Pomodoro timer library with postgres',
+      url='https://github.com/emilyhorsman/nightshades-python',
+      author='Emily Horsman',
+      author_email='me@emilyhorsman.com')
