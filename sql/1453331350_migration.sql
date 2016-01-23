00a5c35872dfb2d6dc5c5ca2e8f9dcd777449bfb
Emily Horsman <me@emilyhorsman.com>
2016-01-23 17:25:02 -0500

    Remove insertions from initial SQL migration.

--- sql/1453331350_migration.sql
+++ sql/1453331350_migration.sql
@@ -20,26 +20,3 @@ CREATE TABLE nightshades.unit_tags (
   unit_id uuid REFERENCES nightshades.units(id),
   string  varchar(40) NOT NULL
 );
-
-INSERT INTO nightshades.users (name) VALUES ('Emily Horsman');
-
-INSERT INTO nightshades.units (user_id, completed, start_time, expiry_time) VALUES
-  (
-    (SELECT id FROM nightshades.users LIMIT 1),
-    TRUE,
-    '2016-01-20 09:05:10 America/Toronto',
-    timestamp '2016-01-20 09:05:10 America/Toronto' + interval '25 minutes'
-  ),
-  (
-    (SELECT id FROM nightshades.users LIMIT 1),
-    TRUE,
-    NOW() - interval '30 minutes',
-    NOW() - interval '5 minutes'
-  ),
-  (
-    (SELECT id FROM nightshades.users LIMIT 1),
-    FALSE,
-    NOW(),
-    NOW() + interval '25 minutes'
-  )
-;
