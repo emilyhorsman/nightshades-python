bed0be11a445380a87c7860a8913bb3a06be2fa5
Emily Horsman <me@emilyhorsman.com>
2016-01-23 17:47:36 -0500

    Import app after load_dotenv call in run.py

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-# Find me in nightshades/http/__init__.py!
 import nightshades
-from nightshades.http import app
 
 if __name__ == '__main__':
     nightshades.load_dotenv()
+
+    # Find me in nightshades/http/__init__.py!
+    from nightshades.http import app
     app.run()
