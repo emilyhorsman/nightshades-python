56e3e1fde28f333ecd7a36f672d9442f79022672
Emily Horsman <me@emilyhorsman.com>
2016-01-27 22:00:53 -0500

    Changed users.id to a uuid, create user provider table

--- sql/1453331350_migration.sql
+++ sql/1453331350_migration.sql
@@ -4,13 +4,13 @@ CREATE SCHEMA nightshades;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE nightshades.users (
-  id   bigserial PRIMARY KEY,
+  id   uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   name varchar(255)
 );
 
 CREATE TABLE nightshades.units (
   id          uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  user_id     integer REFERENCES nightshades.users(id),
+  user_id     uuid REFERENCES nightshades.users(id),
   completed   boolean DEFAULT FALSE NOT NULL,
   start_time  timestamp with time zone NOT NULL DEFAULT NOW(),
   expiry_time timestamp with time zone NOT NULL
