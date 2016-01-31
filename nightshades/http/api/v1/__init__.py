bed50db428efe75cb4f9e60ce87f292292ad80ee
Emily Horsman <me@emilyhorsman.com>
2016-01-31 15:33:47 -0500

    Add CORS header to requests
    If a NIGHTSHADES_CORS environment variable is set, an
    Access-Control-Allow-Origin header will be added to every api.v1 request
    with this value. This allows clients to live on other domains that are
    not http://api.nightshades.xyz

--- nightshades/http/api/v1/__init__.py
+++ nightshades/http/api/v1/__init__.py
@@ -1,5 +1,5 @@
 import peewee
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, current_app
 
 api = Blueprint('api.v1', __name__, url_prefix='/v1')
 
@@ -8,6 +8,15 @@ from nightshades.models import db
 from . import authentication
 from . import endpoints
 from . import errors
+
+
+@api.after_request
+def apply_cors(response):
+    cors = current_app.config.get('CORS', False)
+    if cors:
+        response.headers['Access-Control-Allow-Origin'] = current_app.config.get('CORS')
+
+    return response
 
 
 @api.errorhandler(peewee.DoesNotExist)
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import nightshades
 
 if __name__ == '__main__':
@@ -5,4 +6,8 @@ if __name__ == '__main__':
 
     # Find me in nightshades/http/__init__.py!
     from nightshades.http import app
+    cors = os.environ.get('NIGHTSHADES_CORS', False)
+    if cors:
+        app.config['CORS'] = cors
+
     app.run(host = '0.0.0.0')
